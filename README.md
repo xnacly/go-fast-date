59718c1f0fea6fec057c3221e0858616ecc38487
xnacly <47723417+NaCly@users.noreply.github.com>
2025-11-27 13:02:11 +0100

    benchmarks

--- README.md
+++ README.md
@@ -47,4 +47,22 @@ Joffe's implementation, where applicable.
 ## Benchmarks
 
 Benchmarks focus on comparing `gofastdate.FromUnix` with
-`time.Unix`:
+`time.Unix`. Run with `go test ./... -bench=.`
+
+### Low end hardware:
+
+- micro benchmark: fastdate `~13%` faster
+- stress: fastdate `~4.2x` faster (8.98ms vs 37.38ms)
+
+```text
+goos: linux
+goarch: amd64
+pkg: github.com/xnacly/go-fast-date
+cpu: Intel(R) Core(TM) i5-3210M CPU @ 2.50GHz
+BenchmarkTimeUnix-4                     21141045                56.21 ns/op
+BenchmarkGoFastDateUnix-4               24253924                48.90 ns/op
+BenchmarkStressTimeUnixToDate-4               32             37382839 ns/op              0 B/op          0 allocs/op
+BenchmarkStressGoFastDateFromUnix-4          132              8977516 ns/op              0 B/op          0 allocs/op
+PASS
+ok      github.com/xnacly/go-fast-date  6.369s
+```
--- date_test.go
+++ date_test.go
@@ -1,15 +1,77 @@
 package gofastdate
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
 
-func TestReadme(t *testing.T) {
-	date := FromUnix(time.Now().Unix())
-	fmt.Println(date)
+var sampleDates = []Date{
+	{1970, 1, 1},
+	{1970, 1, 2},
+	{1969, 12, 31},
+	{2000, 1, 1},
+	{1999, 12, 31},
+	{2020, 2, 29},
+	{2025, 11, 27},
+}
 
-	fmt.Println(date.Time())
-	fmt.Println(date.Unix(), date.Time().Unix())
+var sampleUnix = []int64{
+	0,
+	86400,
+	-86400,
+	946684800,
+	915148800,
+	1582934400,
+	1761820800,
+}
+
+func BenchmarkTimeUnix(b *testing.B) {
+	for b.Loop() {
+		for _, u := range sampleUnix {
+			_ = time.Unix(u, 0).UTC().Unix()
+		}
+	}
+}
+
+func BenchmarkGoFastDateUnix(b *testing.B) {
+	for b.Loop() {
+		for _, d := range sampleDates {
+			_ = d.Unix()
+		}
+	}
+}
+
+const stressSize = 1_000_000
+
+var stressUnixTimestamps []int64
+
+func init() {
+	start := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC).Unix()
+	end := time.Date(2100, 12, 31, 0, 0, 0, 0, time.UTC).Unix()
+	stressUnixTimestamps = make([]int64, stressSize)
+
+	for i := 0; i < stressSize; i++ {
+		stressUnixTimestamps[i] = start + int64(i)*(end-start)/stressSize
+	}
+}
+
+func BenchmarkStressTimeUnixToDate(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		for _, u := range stressUnixTimestamps {
+			t := time.Unix(u, 0).UTC()
+			_ = t.Year()
+			_ = t.Month()
+			_ = t.Day()
+		}
+	}
+}
+
+func BenchmarkStressGoFastDateFromUnix(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		for _, u := range stressUnixTimestamps {
+			_ = FromUnix(u)
+		}
+	}
 }
